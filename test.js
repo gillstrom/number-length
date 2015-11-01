d78997e2491f4c1815ecc62a55d7c5c7d8b435d7
Andreas Gillström <andreasgillstrom@gmail.com>
2015-11-01 12:37:33 +0100

    Use `ava` for tests

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 'use strict';
-var assert = require('assert');
-var numberLength = require('./');
+var test = require('ava');
+var fn = require('./');
 
-it('Should return true', function () {
-	assert.strictEqual(numberLength(0), 1);
-	assert.strictEqual(numberLength(9), 1);
-	assert.strictEqual(numberLength(100), 3);
-	assert.strictEqual(numberLength(123.45), 5);
-	assert.strictEqual(numberLength(-1234), 4);
+test('Should return true', function (t) {
+	t.is(fn(0), 1);
+	t.is(fn(9), 1);
+	t.is(fn(100), 3);
+	t.is(fn(123.45), 5);
+	t.is(fn(-1234), 4);
+	t.end();
 });
